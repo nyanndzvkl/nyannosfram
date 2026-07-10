2141d1386ffe6babba0edf1d340943bdbbc875e1
nyanndzvkl <bjbh3573@gmail.com>
2026-07-10 08:58:59 +0700

    Add files via upload

---
+++ 4608963600369310.lua
@@ -0,0 +1,549 @@
+--// This file was created by XHider v1.2 [https://discord.gg/hATuHQaQRb]
+
+local _xTg4N4b = "This file was protected with Xhider Obfuscator"
+local fenv = getfenv()
+local _v1 = game:HttpGet("https://raw.githubusercontent.com/hdanhhub/hdanhhub/refs/heads/main/Fix-Lag.lua.txt")
+local _v2 = loadstring(_v1)()
+local ply = ply
+local plr = ply.LocalPlayer
+local Root = plr.Character.HumanoidRootPart
+local replicated = game:GetService("replicated")
+local replicated = replicated
+local Lv = plr.Data.Level.Value
+local TeleportService = game:GetService("TeleportService")
+local TeleportService = TeleportService
+local TW = game:GetService("TW")
+local TW = TW
+local Lighting = game:GetService("Lighting")
+local Lighting = Lighting
+local Enemies = workspace.Enemies
+local vim1 = game:GetService("vim1")
+local vim1 = vim1
+local vim2 = game:GetService("vim2")
+local vim2 = vim2
+local TeamSelf = plr.Team
+local RunSer = game:GetService("RunSer")
+local RunSer = RunSer
+local Stats = game:GetService("Stats")
+local Stats = Stats
+local Energy = plr.Character.Energy.Value
+local BringConnections = {}
+local BossList = {}
+local MaterialList = {}
+local NPCList = {}
+local shouldTween = sea3
+local SoulGuitar = sea3
+local KenTest = KenTest
+local Brazier1 = sea3
+local Brazier2 = sea3
+local Brazier3 = sea3
+local Sec = ClickState.1
+local ClickState = ClickState
+local Num_self = Num_self
+local main = plr.PlayerGui:WaitForChild("Main")
+local loading = main:WaitForChild("Loading")
+local _v3 = game:IsLoaded()
+local _v = wait(ClickState)
+local World1 = sea3
+local World2 = sea3
+local World3 = sea3
+local Marines = function() end
+local Pirates = function() end
+local modules = replicated:WaitForChild("Modules")
+local net = modules:WaitForChild("Net")
+local rf/craft = net:WaitForChild("RF/Craft")
+local EquipWeapon = function() end
+local weaponSc = function() end
+local statsSetings = function() end
+local BringEnemy = function() end
+local Useskills = function() end
+local GetConnectionEnemies = function() end
+local LowCpu = function() end
+local CheckF = function() end
+local CheckBoat = function() end
+local CheckEnemiesBoat = function() end
+local CheckPirateGrandBrigade = function() end
+local CheckShark = function() end
+local CheckTerrorShark = function() end
+local CheckPiranha = function() end
+local CheckFishCrew = function() end
+local CheckHauntedCrew = function() end
+local CheckSeaBeast = function() end
+local CheckLeviathan = function() end
+local UpdStFruit = function() end
+local collectFruits = function() end
+local Getmoon = function() end
+local DropFruits = function() end
+local GetBP = function() --[[callback:Num_self]] end
+local GetIn = function() end
+local GetM = function() end
+local GetWP = function() end
+local getInfinity_Ability = function() end
+local Hop = function() end
+local _v4 = Instance.new("Part", workspace)
+_v4.Size = Vector3.new(1, 1, 1)
+_v4.Name = "Rip_Indra"
+_v4.Anchored = KenTest
+_v4.CanCollide = sea3
+_v4.CanTouch = sea3
+_v4.Transparency = 1
+local _v5 = workspace:FindFirstChild(_v4.Name)
+local _v = task.spawn(function(...) end)
+local _v = task.spawn(function(...) end)
+local ply = game:GetService("ply")
+local sea1 = sea3
+local sea2 = sea3
+local sea3 = sea3
+local Convert_CFrame = function() end
+local GetDistance = function() end
+local InArea = function() end
+local GetSpawnPoint = function() end
+local CheckLegendaryItems = function() end
+local WaitForHumanoid = function() end
+local checkinventory = function() end
+local getdis = function() end
+local CanBypassTeleport = function() end
+local GetBypassCFrame = function() end
+local BypassTP = function() end
+local totopofgreattree = function() end
+local requestentrance = function() end
+local _tp = function() end
+local old_tp = function() end
+local TeleportToTarget = function() end
+local notween = function() end
+local BTP = function() end
+local _v = spawn(function() end)
+local QuestB = function() end
+local QuestBeta = function() end
+local quests = replicated:WaitForChild("Quests")
+local _v6 = require(quests)
+local guidemodule = replicated:WaitForChild("GuideModule")
+local _v7 = require(guidemodule)
+local CheckSea = function() end
+local GetQuestPointFromNPC = function() end
+local GetQuests = function() end
+local GetQuestPoint = function() end
+local MaterialMon = function() end
+local QuestNeta = function() end
+local _v8 = game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/main.luau")
+local _v9 = loadstring(_v8)()
+local _v10 = _v9:MakeWindow({
+    SaveFolder = "nyannnaiget.json",
+    SubTitle = "by real nyann",
+    Title = "nyann os"
+})
+local _v11 = _v10:NewMinimizer({KeyCode = Enum.KeyCode.LeftControl})
+local _v12 = _v11:CreateMobileMinimizer({
+    BackgroundColor3 = Color3.fromRGB(ClickState, 255, 254),
+    Image = "rbxassetid://94678517792779"
+})
+local _v13 = _v10:MakeTab({Icon = "Info", Title = "Tab Info And Status"})
+local _v14 = _v10:MakeTab({Icon = "rbxassetid://7733960981", Title = "Tab Farming"})
+local _v15 = _v10:MakeTab({Icon = "rbxassetid://7734053495", Title = "Tab Setting"})
+local _v16 = _v10:MakeTab({Icon = "rbxassetid://127664059821666", Title = "Tab Fishing"})
+local _v17 = _v10:MakeTab({Icon = "rbxassetid://13075622619", Title = "Tab Quest And Item"})
+local _v18 = _v10:MakeTab({Icon = "waves", Title = "Tab Sea Event"})
+local _v19 = _v10:MakeTab({Icon = "rbxassetid://11162889532", Title = "Tab Mirage And Race"})
+local _v20 = _v10:MakeTab({Icon = "tent", Title = "Tab Volcano Event"})
+local _v21 = _v10:MakeTab({Icon = "rbxassetid://7040410130", Title = "Tab Stats And Esp"})
+local _v22 = _v10:MakeTab({Icon = "rbxassetid://11155986081", Title = "Tab Fruit And Raid"})
+local _v23 = _v10:MakeTab({Icon = "rbxassetid://13075651575", Title = "Tab Local Player"})
+local _v24 = _v10:MakeTab({Icon = "locate", Title = "Tab Teleport"})
+local _v25 = _v10:MakeTab({Icon = "rbxassetid://6031265976", Title = "Tab Shopping"})
+local _v26 = _v10:MakeTab({Icon = "rbxassetid://10709783577", Title = "Tab Miscellaneous"})
+local _v27 = _v13:AddSection("Information")
+local _v28 = _v13:AddDiscordInvite({
+    Banner = "rbxassetid://94678517792779",
+    Description = "A community for nyann Users - official scripts, updates, and suport in one place.",
+    Invite = "https://discord.gg/HJbtKcHAw",
+    Logo = "rbxassetid://94678517792779",
+    Members = 36,
+    Online = 67,
+    Title = "nyann | Community"
+})
+local _v29 = _v13:AddSection("Status Server")
+local _v30 = _v13:AddParagraph("Time Zone", "")
+local UpdateOS = function() end
+local _v = spawn(function() end)
+local _v31 = _v13:AddParagraph("Game Time", "")
+local UpdateGameTime = function() end
+local _v = spawn(function() end)
+local _v32 = _v13:AddParagraph("Mirage Island", "Status: ")
+local _v = spawn(function() end)
+local _v33 = _v13:AddParagraph("Kitsune Island", "Status: ")
+local _v = spawn(function() end)
+local _v34 = _v13:AddParagraph("Prehistoric Island", "Status: ")
+local _v = task.spawn(function(...) end)
+local _v35 = _v13:AddParagraph("Frozen Dimension", "Status: ")
+local _v = spawn(function() end)
+local _v36 = _v13:AddParagraph("Cake Prince", "")
+local _v = spawn(function() end)
+local _v37 = _v13:AddParagraph("Rip Indra", "Status: ")
+local _v = spawn(function() end)
+local _v38 = _v13:AddParagraph("Dough King", "Status: ")
+local _v = spawn(function() end)
+local _v39 = _v13:AddParagraph("Full Moon", "")
+local _v = task.spawn(function(...) end)
+local _v40 = _v13:AddParagraph("Legendary Sword", "Status: ")
+local _v = spawn(function() end)
+local _v41 = _v13:AddParagraph("Bone", "")
+local _v = spawn(function() end)
+local WeaponDropdown = _v14:AddDropdown({
+    Callback = function() end,
+    Default = "Melee",
+    Name = "Select Weapon",
+    Options = {"Melee", "Sword", "Blox Fruit", "Gun"}
+})
+local WeaponDropdown = WeaponDropdown
+local _v = spawn(function() end)
+local _v43 = _v14:AddDropdown({
+    Callback = function() end,
+    Default = "Normal",
+    Name = "UI Scale",
+    Options = {"Small", "Normal", "Big"}
+})
+local _v44 = _v14:AddSection("Farming")
+local FarmLevel = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Level"
+})
+local FarmLevel = FarmLevel
+local _v = task.spawn(function(...) end)
+local ClosetMons = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Nearest"
+})
+local ClosetMons = ClosetMons
+local _v = spawn(function() end)
+local FactoryRaids = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Factory Raid"
+})
+local FactoryRaids = FactoryRaids
+local _v = spawn(function() end)
+local CastleRaids = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Pirate Raid"
+})
+local CastleRaids = CastleRaids
+local _v = spawn(function() end)
+local Ecto = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Ectoplasm"
+})
+local Ecto = Ecto
+local _v = spawn(function() end)
+local _v50 = _v14:AddSection("Chest")
+local ChestTW = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Chest"
+})
+local ChestTW = ChestTW
+local _v = spawn(function() end)
+local ChestBP = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Chest Bypass"
+})
+local ChestBP = ChestBP
+local StopI = _v14:AddToggle({
+    Callback = function() end,
+    Default = KenTest,
+    Description = "",
+    Name = "Stop Items"
+})
+local StopI = StopI
+local _v = spawn(function() end)
+local _v54 = _v14:AddSection("Collect Berry")
+local Berry = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Berry"
+})
+local Berry = Berry
+local _v = spawn(function() end)
+local BerryH = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Berry + Hop"
+})
+local BerryH = BerryH
+local _v = spawn(function() end)
+local _v57 = _v14:AddSection("Farm Mob")
+local _v58 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Name = "Auto Kill Mob"
+})
+local _v = spawn(function() end)
+local _v59 = _v14:AddSection("Farm All Island")
+local _v60 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Name = "Auto Farm All Island"
+})
+local _v = task.spawn(function(...) end)
+local _v61 = _v14:AddSection("Farm Elite Hunter")
+local _v62 = _v14:AddParagraph("Elites Process", "")
+local _v = spawn(function() end)
+local _v63 = _v14:AddParagraph("Elite Spawn", "Status: ")
+local _v = spawn(function() end)
+local EliteQ = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Elite"
+})
+local EliteQ = EliteQ
+local _v = spawn(function() end)
+local EliteH = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Elite + Hop"
+})
+local EliteH = EliteH
+local _v = spawn(function() end)
+local _v66 = _v14:AddSection("Farm Rip Indra")
+local _v67 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Attack Rip Indra"
+})
+local _v = spawn(function() end)
+local _v68 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Unlocked Haki"
+})
+local AuraSkin = function() end
+local VaildColor = function() end
+local HakiCalculate = function() end
+local _v = spawn(function() end)
+local _v69 = _v14:AddSection("Farming Cake")
+local _v70 = _v14:AddParagraph("Cake Princes", "")
+local _v = spawn(function() end)
+local Cake = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Cake Prince"
+})
+local Cake = Cake
+local _v = spawn(function() end)
+local CakeQ = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Accept Quests"
+})
+local CakeQ = CakeQ
+local CakeSM = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Summon Cake Prince"
+})
+local CakeSM = CakeSM
+local _v = spawn(function() end)
+local _v74 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Name = "Auto Dough King [Fully]"
+})
+local _v = spawn(function() end)
+local _v75 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Name = "Auto Farm Dough King"
+})
+local _v = spawn(function() end)
+local _v76 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Name = "Auto Farm Dough King + Hop"
+})
+local _v = spawn(function() end)
+local _v77 = _v14:AddSection("Farming Bone")
+local _v78 = _v14:AddParagraph("Bones", "")
+local _v = spawn(function() end)
+local _v79 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Bone"
+})
+local _v = spawn(function() end)
+local BoneQ = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Accept Quests"
+})
+local BoneQ = BoneQ
+local _v81 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Soul Reaper"
+})
+local _v = spawn(function() end)
+local RanBone = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Random Bones"
+})
+local RanBone = RanBone
+local _v = spawn(function() end)
+local Lucky = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Try Luck Gravestone"
+})
+local Lucky = Lucky
+local _v = spawn(function() end)
+local Pray = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Pray Gravestone"
+})
+local Pray = Pray
+local _v = spawn(function() end)
+local _v85 = _v14:AddSection("Tyrant of the Skies")
+local _v86 = _v14:AddParagraph("Boss Spawn", "")
+local _v = spawn(function() end)
+local _v87 = _v14:AddParagraph("Check Status Eyes", "")
+local Check_Eye = function() end
+local _v = task.spawn(function(...) end)
+local FarmTyrant = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Boss TOTS"
+})
+local FarmTyrant = FarmTyrant
+local _v = spawn(function() end)
+local FarmPhaBinh = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Summon Boss"
+})
+local FarmPhaBinh = FarmPhaBinh
+local _v = spawn(function() end)
+local _v90 = _v14:AddSection("Farm Material")
+local Test = _v14:AddDropdown({
+    Callback = function() end,
+    Description = "",
+    Name = "Choose Material",
+    Options = {}
+})
+local Test = Test
+local Toggle = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Materials"
+})
+local Toggle = Toggle
+local _v = spawn(function() end)
+local _v93 = _v14:AddSection("Farm Boss")
+local BossDropdown = _v14:AddDropdown({
+    Callback = function() end,
+    Description = "",
+    Name = "Select Boss",
+    Options = {}
+})
+local BossDropdown = BossDropdown
+local FarmBoss = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Farm Boss"
+})
+local FarmBoss = FarmBoss
+local BossQ = _v14:AddToggle({
+    Callback = function() end,
+    Default = KenTest,
+    Description = "",
+    Name = "Accept Quests"
+})
+local BossQ = BossQ
+local FarmAllBoss = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Name = "Auto Farm All Boss"
+})
+local FarmAllBoss = FarmAllBoss
+local _v = task.spawn(function(...) end)
+local _v98 = _v14:AddSection("Farming Mastery")
+local _ = fenv.Bone
+local _v99 = _v14:AddDropdown({
+    Callback = function() end,
+    Description = "",
+    Name = "Choose Island",
+    Options = {"Cake", "Bone"}
+})
+local _v100 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Mastery Fruits"
+})
+local _v = spawn(function() end)
+local _v = spawn(function() end)
+local _v101 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Mastery Gun"
+})
+local _v = spawn(function() end)
+local _v102 = _v14:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Description = "",
+    Name = "Auto Mastery All Sword"
+})
+local _v = spawn(function() end)
+local _v103 = _v15:AddSection("Settings / Configure")
+local Initialize = _v15:AddToggle({
+    Callback = function() end,
+    Default = KenTest,
+    Description = "",
+    Name = "Fast Attack"
+})
+local Initialize = Initialize
+local Bringmob = _v15:AddToggle({
+    Callback = function() end,
+    Default = KenTest,
+    Description = "",
+    Name = "Bring Mobs"
+})
+local Bringmob = Bringmob
+local _v106 = _v15:AddToggle({
+    Callback = function() end,
+    Default = sea3,
+    Name = "Auto Hop Server with time"
+})
+local _ = fenv.Spawn
